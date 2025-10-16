42f3d478e6ac044a74a4e516d1a729fd92c57250
rgcastrof <rogeriogirao1@proton.me>
2025-10-16 16:21:20 -0300

    implementa funcionalidade 2 da apirest

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,7 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, HTTPException
 from app.database.mini_db import MiniDb
-from app.models import User
+from app.models import User, CountResponse
 from datetime import datetime
+from pydantic import BaseModel
+from typing import List
+
+class UsersPaginationResponse(BaseModel):
+    page: int
+    page_size: int
+    total: int
+    users: List[User]
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -16,3 +24,18 @@ async def create_user(user: User):
     user_data["register_date"] = datetime.utcnow().isoformat()
     new_id = db.insert(user_data)
     return User(id=new_id, **user_data)
+
+@router.get("/", response_model=UsersPaginationResponse)
+async def get_users(page: int = Query(1, ge=1), page_size: int = Query(5, ge=1)):
+    all_users = db.read()
+    start = (page - 1) * page_size
+    end = start + page_size
+    paginated_users = [User(**u) for u in all_users[start:end]]
+    total = len(all_users)
+
+    return {
+        "page": page,
+        "page_size": page_size,
+        "total": total,
+        "users": paginated_users
+    }
