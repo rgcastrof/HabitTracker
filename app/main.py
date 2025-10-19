a1ab8774b317daa9ce3b8f992a458ee91b8ce1cf
rgcastrof <rogeriogirao1@proton.me>
2025-10-18 21:04:28 -0300

    adiciona router_utils para ajudar a lidar com as funcionalidades

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from app.routers.user import router as user_router
+from app.routers.habit import router as habit_router
+from app.utils.router_utils import router as export_router
 
 app = FastAPI()
+app.include_router(export_router)
 app.include_router(user_router)
+app.include_router(habit_router)
 
 @app.get("/")
 async def root():
