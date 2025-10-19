cd0f257381df3a944f6549b797a04acbf49bda05
rgcastrof <rogeriogirao1@proton.me>
2025-10-18 21:02:16 -0300

    adiciona novas classes pydantic ao model habit para serem usadas nas rotas

--- app/models/habit.py
+++ app/models/habit.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 from datetime import datetime
+
+class HabitCreate(BaseModel):
+    user_id: int
+    name: str
+    description: str
+    frequence: str
 
 class Habit(BaseModel):
     id: Optional[int] = None
@@ -9,3 +15,16 @@ class Habit(BaseModel):
     description: str
     frequence: str
     creation_date: Optional[datetime] = None
+    deleted: bool
+
+    class Config:
+        from_atributes = True
+
+class HabitsPaginationResponse(BaseModel):
+    page: int
+    page_size: int
+    total: int
+    habits: List[Habit]
+
+class HabitsCountResponse(BaseModel):
+    total_habits: int
