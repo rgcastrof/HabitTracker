553ee26e3e50f9c61a9c70aec2b08581896a84d4
rgcastrof <rogeriogirao1@proton.me>
2025-11-25 14:50:36 -0300

    remove APP_NAME nao usado

--- app/core/config.py
+++ app/core/config.py
@@ -2,10 +2,9 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    APP_NAME: str
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
 
-settings = Settings() # type: ignore[reportCallIssue]
+settings = Settings()
