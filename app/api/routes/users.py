b67d085d164f42dffbb69c49ca87861e3d1c7cee
rgcastrof <rogeriogirao1@proton.me>
2025-11-27 14:54:49 -0300

    adiciona update do crud de user

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
-from app.schemas.user import UserCreate, UserRead
+from app.schemas.user import UserCreate, UserRead, UserUpdate
 from app.models.user import User
 from sqlmodel import Session, select
 from app.core.database import get_session
@@ -38,3 +38,33 @@ async def get_user(user_id: int, db: Session = Depends(get_session)):
     if not user:
         raise HTTPException(404, detail="Usuário não encontrado")
     return user
+
+# Update
+@router.patch("/{user_id}", response_model=UserRead)
+async def update_user(user_id: int, user_update: UserUpdate, db: Session = Depends(get_session)):
+    user = db.get(User, user_id)
+    if not user:
+        raise HTTPException(404, detail="Usuário não encontrado")
+
+    if user_update.name is not None:
+        if user_update.name.strip() == "":
+            raise HTTPException(status_code=400, detail="Nome não pode ser vazio")
+        user.name = user_update.name
+    if user_update.email is not None:
+        if user_update.email.strip() == "":
+            raise HTTPException(status_code=400, detail="Email não pode ser vazio")
+        user.email = user_update.email
+    if user_update.password is not None:
+        if user_update.password.strip() == "":
+            raise HTTPException(status_code=400, detail="Senha não pode ser vazio")
+        user.password = user_update.password
+
+    try:
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+        return user
+    except Exception as e:
+        db.rollback()
+        print(f"Erro ao atualizar dados do usuário: {e}")
+        raise HTTPException(status_code=500, detail="Erro ao atualizar dados do usuário")
