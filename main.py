b9a2c84b1c6562f56032f2899dae0faafa87409f
rgcastrof <rogeriogirao1@proton.me>
2025-11-27 14:04:51 -0300

    inclui rota principal na main

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.core.database import create_db_and_tables
+from app.api.router import router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     create_db_and_tables()
     yield
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(title="Habit Tracker API", lifespan=lifespan)
+app.include_router(router)
 
 @app.get("/")
-def home():
-    return {"msg": "Habit Tracker API funcionando" }
+def root():
+    return {"message": "Habit Tracker API is still working"}
