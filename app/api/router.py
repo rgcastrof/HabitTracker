4a8b279e52bce702d056f7239bfb530e82ccd680
rgcastrof <rogeriogirao1@proton.me>
2025-11-29 12:19:56 -0300

    finaliza crud inicial de habit

--- app/api/router.py
+++ app/api/router.py
@@ -4,3 +4,4 @@ from app.api.routes import users, habits, records
 
 router = APIRouter()
 router.include_router(users.router, prefix="/users", tags=["Users"])
+router.include_router(habits.router, prefix="/habits", tags=["Habits"])
