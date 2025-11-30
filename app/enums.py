a6e504adeefe2b9346de4defcad634a30d04013a
rgcastrof <rogeriogirao1@proton.me>
2025-11-30 15:54:04 -0300

    add docstring para enums.py

--- app/enums.py
+++ app/enums.py
@@ -1,11 +1,29 @@
 from enum import Enum
 
 class Frequence(str, Enum):
+    """
+    Freqûencia de execução de um hábito
+
+    Attributes:
+        DAILY: Executado diariamente
+        WEEKLY: Executado semanalmente
+        MONTHLY: Executado mensalmente
+    """
+
     DAILY = "daily"
     WEEKLY = "weekly"
     MONTHLY = "monthly"
 
 class Status(str, Enum):
+    """
+    Status de conclusão de um hábito
+
+    Attributes:
+        FINISHED: Atividade concluída
+        PARTIALLY: Atividade parcialmente concluída
+        UNFINISHED: Atividade não concluída
+    """
+
     FINISHED = "completed"
     PARTIALLY = "partially"
     UNFINISHED = "unfinished"
