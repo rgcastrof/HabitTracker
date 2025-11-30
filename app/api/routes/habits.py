ec7757c1d032c79c1660c55c8ffa056caaf01a5d
rgcastrof <rogeriogirao1@proton.me>
2025-11-30 08:11:19 -0300

    adiciona busca filtrada por relacionamento e busca por texto

--- app/api/routes/habits.py
+++ app/api/routes/habits.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.exc import SQLAlchemyError
-from sqlmodel import Session
+from sqlalchemy import func
+from sqlmodel import Session, select
 from app.core.database import get_session
 from app.models.habit import Habit
+from app.models.record import Record
 from app.models.user import User
 from app.schemas.habit import HabitCreate, HabitRead, HabitUpdate
 from app.crud.base import logger
+from app.schemas.record import RecordRead
 
 from app.crud.base import CRUDBase
 
@@ -36,6 +39,26 @@ def get_all_habits(
 ):
     return crud_habit.get_all(offset=offset, limit=limit, db=db)
 
+@router.get("/search", response_model=list[HabitRead])
+def get_habit_by_text(
+    q: str = "",
+    offset: int = Query(default=0),
+    limit: int = Query(default=10),
+    db: Session = Depends(get_session),
+    ):
+    try:
+        if q:
+            stmt = select(Habit).where(func.lower(Habit.name).like(f"%{q.lower()}%"))
+        else:
+            stmt = select(Habit)
+        stmt = stmt.offset(offset).limit(limit)
+        return db.exec(stmt).all()
+    except SQLAlchemyError as e:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Consulta nao encontrada: {e}"
+        )
+
 @router.get("/{habit_id}", response_model=HabitRead)
 def get_habit(habit_id: int, db: Session = Depends(get_session)):
     getted_habit = crud_habit.get_by_id(habit_id, db)
@@ -55,3 +78,19 @@ def delete_habit(habit_id: int, db: Session = Depends(get_session)):
     deleted_habit = crud_habit.delete(habit_id, db)
     if not deleted_habit:
         raise HTTPException(status_code=500, detail="Erro ao deletar hábito")
+
+@router.get("/{habit_id}/records", response_model=list[RecordRead])
+def get_records_by_habit(
+    habit_id: int,
+    offset: int = Query(default=0),
+    limit: int = Query(default=10),
+    db: Session = Depends(get_session)
+):
+    try:
+        stmt = select(Record).join(Habit).where(Habit.id == habit_id).offset(offset).limit(limit)
+        return db.exec(stmt).all()
+    except SQLAlchemyError as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Erro ao buscar registros associados a habito com id {habit_id}: {e}"
+        )
