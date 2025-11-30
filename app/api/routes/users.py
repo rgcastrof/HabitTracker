7727e6c725e574d90ae0f75c76d02f593e6665b8
rgcastrof <rogeriogirao1@proton.me>
2025-11-30 08:10:55 -0300

    adiciona busca filtrada por relacionamento

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlalchemy.exc import SQLAlchemyError
+from app.models.habit import Habit
 from app.schemas.user import UserCreate, UserRead, UserUpdate
 from app.models.user import User
-from sqlmodel import Session
+from app.schemas.habit import HabitRead
+from sqlmodel import Session, select
 from app.core.database import get_session
 from app.crud.base import CRUDBase
 
@@ -20,8 +23,8 @@ def create_user(user: UserCreate, db: Session = Depends(get_session)):
 # Read (all)
 @router.get("/", response_model=list[UserRead])
 def get_all_users(
-    offset=Query(default=0, ge=0),
-    limit=Query(default=10, le=100),
+    offset=Query(default=0),
+    limit=Query(default=10),
     db: Session = Depends(get_session)
 ):
     return crud_user.get_all(offset=offset, limit=limit, db=db)
@@ -48,3 +51,21 @@ def delete_user(user_id: int, db: Session = Depends(get_session)):
     deleted_user = crud_user.delete(user_id, db)
     if not deleted_user:
         raise HTTPException(status_code=500, detail="Erro ao deletar usuário")
+
+
+#  Consultas complexas
+@router.get("/{user_id}/habits", response_model=list[HabitRead])
+def get_habits_by_user(
+    user_id: int,
+    offset: int = Query(default=0),
+    limit: int = Query(default=10),
+    db: Session = Depends(get_session)
+):
+    try:
+        stmt = select(Habit).join(User).where(User.id == user_id).offset(offset).limit(limit)
+        return db.exec(stmt).all()
+    except SQLAlchemyError as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Erro ao buscar habitos associados a usuario com id {user_id}: {e}"
+        )
