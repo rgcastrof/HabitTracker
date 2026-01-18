b3f943c7c4e60f1f84d708d902758fa62fd9443a
rgcastrof <rogeriogirao1@proton.me>
2026-01-18 20:41:44 -0300

    adiciona consulta de registros por data

--- api/routes/records.py
+++ api/routes/records.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from beanie import PydanticObjectId
 from fastapi import APIRouter, HTTPException
 from fastapi_pagination import Page
@@ -31,38 +32,6 @@ async def create_record(habit_id: PydanticObjectId, record_in: RecordCreate) -> 
 
     record = Record(**record_in.model_dump(), habit=habit)  # type: ignore
     await record.insert()
-    return RecordRead.model_validate(record.model_dump())
-
-# Read
-@router.get("/", response_model=Page[RecordRead])
-async def get_records() -> Page[RecordRead]:
-    """
-    Retorna uma lista paginada de registros.
-
-    Returns:
-        Page[RecordRead]: Página contendo a lista de registros.
-    """
-    return await apaginate(Record.find_all())
-
-@router.get("/{record_id}", response_model=RecordRead)
-async def get_record(record_id: PydanticObjectId) -> RecordRead:
-    """
-    Retorna um registro específico pelo seu identificador.
-
-    Args:
-        record_id (PydanticObjectId): Identificador único do registro.
-
-    Returns:
-        RecordRead: Registro correspondente ao ID informado.
-
-    Raises:
-        HTTPException:
-            - 404: Registro não encontrado.
-    """
-    record = await Record.get(record_id)
-    if not record:
-        raise HTTPException(status_code=404, detail="Record not found")
-
     return RecordRead.model_validate(record.model_dump())
 
 # Update
@@ -115,3 +84,71 @@ async def delete_record(record_id: PydanticObjectId) -> dict:
         raise HTTPException(status_code=404, detail="Record not found")
     await record.delete()
     return {"message": f"Record with id: {record_id} deleted."}
+
+# Consultas
+# Consulta por data
+@router.get("/date", response_model=Page[RecordRead])
+async def get_records_by_date(
+    year: int,
+    month: int,
+    start: int,
+    end: int
+) -> Page[RecordRead]:
+    """
+    Retorna registros do banco de dados filtrados por intervalo de datas.
+
+    Args:
+        year (int): Ano da data de criação dos registros.
+        month (int): Mês da data de criação dos registros.
+        start (int): Dia inicial do intervalo de busca.
+        end (int): Dia final do intervalo de busca.
+
+    Returns:
+        Page[RecordRead]: Lista paginada de registros encontrados no intervalo informado.
+
+    Raises:
+        HTTPException:
+            - 400: Intervalo de datas inválido.
+    """
+    start_datetime = datetime(year, month, start, tzinfo=timezone.utc)
+    end_datetime = datetime(year, month, end, tzinfo=timezone.utc)
+    records = await apaginate(Record.find(
+        {"creation_date": {
+            "$gte": start_datetime,
+            "$lte": end_datetime
+        }}
+    ))
+    return records
+
+
+# Read
+@router.get("/", response_model=Page[RecordRead])
+async def get_records() -> Page[RecordRead]:
+    """
+    Retorna uma lista paginada de registros.
+
+    Returns:
+        Page[RecordRead]: Página contendo a lista de registros.
+    """
+    return await apaginate(Record.find_all())
+
+@router.get("/{record_id}", response_model=RecordRead)
+async def get_record(record_id: PydanticObjectId) -> RecordRead:
+    """
+    Retorna um registro específico pelo seu identificador.
+
+    Args:
+        record_id (PydanticObjectId): Identificador único do registro.
+
+    Returns:
+        RecordRead: Registro correspondente ao ID informado.
+
+    Raises:
+        HTTPException:
+            - 404: Registro não encontrado.
+    """
+    record = await Record.get(record_id)
+    if not record:
+        raise HTTPException(status_code=404, detail="Record not found")
+
+    return RecordRead.model_validate(record.model_dump())
